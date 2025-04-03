7b592e6b5a43cbbe529c0e0cf8e718f48f28c0d2
Kimjunyeop <kjohn0714@ajou.ac.kr>
2025-04-03 12:37:31 +0900

    prototype - input argument condition branch

--- SSD/main.c
+++ SSD/main.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
 // 전체적인 LOGIC
 
-// 경우에 따라 1,2개의 argument를 받아 수행한다.
+// 경우에 따라 2, 3개의 argument를 받아 수행한다.
 // read의 경우 1개의 argument를 받는다.
 // write의 경우 2개의 argument를 받는다.
 
@@ -22,12 +21,28 @@
 // read, write가 실행 될 경우 각각
 // RESULT가 차 있으면(bool isReaded), RESULT 배열을 ./result.txt에 쓰고 저장한다(덮어쓰기 x).
 // WRITE가 되었으면(bool isWrited), NAND 배열을 ./nand.txt에 쓰고 저장한다(덮어쓰기 x).
-
+#include <stdio.h>
+#include <string.h>
+#define INVALID1 printf("INVALID : ARGUMENT ERROR")
+#define INVALID2 printf("INVALID : COMMAND ERROR")
 int main(int argc, char* argv[]) {
 	//printf("argc = %d\n", argc);
-	for (int i = 0; i < argc; i++) {
+	/*for (int i = 0; i < argc; i++) {
 		printf("argv[%d] = %s\n", i, argv[i]);
+	}*/
+	if (argc < 2 || argc > 3) {
+		INVALID1;
 	}
-
+	else {
+		if (strcmp(argv[1], "READ") == 0) { // + argument 2인지 확인!
+			printf("READING\n");
+		}
+		else if (strcmp(argv[1], "WRITE") == 0) { // + argument 3인지 확인!
+			printf("WRITING\n");
+		}
+		else {
+			INVALID2;
+		}
+	}
 	return 0;
 }
